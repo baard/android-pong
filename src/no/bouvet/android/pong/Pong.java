459e9cb0363c2595a67b990793b977863845fbe7
Baard H. Rehn Johansen <baard@baard-d610.(none)>
2009-06-11 22:19:56 +0200

    initial

---
+++ src/no/bouvet/android/pong/Pong.java
@@ -0,0 +1,13 @@
+package no.bouvet.android.pong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pong extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
